89ee52f4551c8bfd570339901dcb496642b3e281
mattlevine22 <mattlevine22@gmail.com>
2022-08-02 16:46:08 -0700

    fixing output directories for hpc scripts

--- run_scripts/inference_HPC/infer_univ485.sh
+++ run_scripts/inference_HPC/infer_univ485.sh
@@ -10,8 +10,8 @@
 module load python3/3.7.0
 module load gcc/9.2.0
 
-round_dir="/groups/astuart/mlevine/benbio/results/true_inference"
+round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/true_inference"
 
 echo "Sending results to $round_dir"
-srun --ntasks=1 python3 inference_tests_3m_univ485.py --maxiter_K 15 --popsize_K 50 --base_dir "$round_dir/univ485_15x50_15x50"
+srun --ntasks=1 python3 inference_tests_3m_univ485.py --n_weights_per_round 10 --maxiter_K 15 --popsize_K 50 --base_dir "$round_dir/univ485_15x50_15x50_10targets"
 wait
