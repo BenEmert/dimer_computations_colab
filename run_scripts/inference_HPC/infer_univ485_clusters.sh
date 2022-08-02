4f3030efea2f39a0e2b99075c8412afde5fcb3e9
mattlevine22 <mattlevine22@gmail.com>
2022-08-02 15:52:56 -0700

    add run for fitting to clusters from universe 485

---
+++ run_scripts/inference_HPC/infer_univ485_clusters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH -J "benbio"   # job name
+#SBATCH --output=slurm/%x.%j.out
+#SBATCH --error=slurm/%x.%j.err
+
+module load python3/3.7.0
+module load gcc/9.2.0
+
+round_dir="/groups/astuart/mlevine/benbio/results/true_inference"
+
+echo "Sending results to $round_dir"
+srun --ntasks=1 python3 metacluster_inference_tests_1.py --n_weights_per_round 10 --maxiter_K 15 --popsize_K 50 --base_dir "$round_dir/univ485_clusters_15x50_15x50_10targets"
+wait
