88955b294cbbf3247781113f62ec6ece24c84891
mattlevine22 <mattlevine22@gmail.com>
2022-08-03 18:34:40 -0700

    include metacluster HPC script

---
+++ run_scripts/inference_HPC/infer_metaclusters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Submit this script with: sbatch thefilename
+
+#SBATCH --time=48:00:00   # walltime
+#SBATCH -J "benbio"   # job name
+#SBATCH --output=slurm/%x.%j.out
+#SBATCH --error=slurm/%x.%j.err
+
+module load python3/3.7.0
+module load gcc/9.2.0
+
+round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/metaclusters"
+
+echo "Sending results to $round_dir"
+srun --ntasks=1 python3 metacluster_inference_tests_1.py --n_targets_per_round 100 --maxiter_O 5 --popsize_O 10 --maxiter_K 10 --popsize_K 25 --base_dir "$round_dir/inner5x10_outer10x25_100targets"
+wait
--- run_scripts/inference_HPC/infer_univ485_clusters.sh
+++ run_scripts/inference_HPC/infer_univ485_clusters.sh
@@ -7,11 +7,11 @@
 #SBATCH --output=slurm/%x.%j.out
 #SBATCH --error=slurm/%x.%j.err
 
-module load python3/3.7.0
-module load gcc/9.2.0
+# module load python3/3.7.0
+# module load gcc/9.2.0
 
 round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/true_inference"
 
 echo "Sending results to $round_dir"
-srun --ntasks=1 python3 metacluster_inference_tests_1.py --n_weights_per_round 10 --maxiter_K 15 --popsize_K 50 --base_dir "$round_dir/univ485_clusters_15x50_15x50_10targets"
+srun --ntasks=1 python3 inference_tests_3m_univ485_cluster.py --n_weights_per_round 10 --maxiter_K 15 --popsize_K 50 --base_dir "$round_dir/univ485_clusters_15x50_15x50_10targets"
 wait
