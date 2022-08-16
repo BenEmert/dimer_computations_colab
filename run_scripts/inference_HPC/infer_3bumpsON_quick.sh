c8bdb8dc03d1522c35de67a5016e02656acc416e
mattlevine22 <mattlevine22@gmail.com>
2022-08-16 09:43:29 -0600

    fix folder name

--- run_scripts/inference_HPC/infer_3bumpsON_quick.sh
+++ run_scripts/inference_HPC/infer_3bumpsON_quick.sh
@@ -12,7 +12,7 @@
 # module load python3/3.7.0
 # module load gcc/9.2.0
 
-round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/3bumps_eps1e-16"
+round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/3bumpsON_eps1e-16"
 
 echo "Sending results to $round_dir"
 srun --ntasks=1 python3 bump_inference_tests_ainner_3bump_on_Library.py --m 3 --maxiter_O 10 --popsize_O 3 --maxiter_K 10 --popsize_K 25 --base_dir "$round_dir/a-inner_w-inner_m3/inner10x3_outer10x25" &
