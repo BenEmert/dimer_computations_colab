373572939f63579dda6daad6cf67d8ef07831627
mattlevine22 <mattlevine22@gmail.com>
2022-08-22 16:28:43 -0700

    update output name

--- run_scripts/inference_HPC/infer_6bumps_mixed.sh
+++ run_scripts/inference_HPC/infer_6bumps_mixed.sh
@@ -12,7 +12,7 @@
 # module load python3/3.7.0
 # module load gcc/9.2.0
 
-round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/6bumps_mixed_eps1e-16_singlebeta"
+round_dir="/groups/astuart/mlevine/dimer_computations_colab/results/6bumps_mixed_eps1e-16_singlebeta_percentiles_v2"
 
 echo "Sending results to $round_dir"
 srun --ntasks=1 python3 bump_inference_tests_ainner_6bump_mixed_Library.py --single_beta 1 --maxiter_O 10 --popsize_O 3 --maxiter_K 10 --popsize_K 25 --base_dir "$round_dir/a-inner_w-inner_m3/inner10x3_outer10x25" &
