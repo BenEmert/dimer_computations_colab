7fc058402551f9a57f5e17c5dbf82a89f3f64886
mattlevine22 <mattlevine22@gmail.com>
2022-09-17 15:47:08 -0700

    fixes for newly installed bugs

--- run_scripts/bump_inference_tests_ainner_ManyBumps_main.py
+++ run_scripts/bump_inference_tests_ainner_ManyBumps_main.py
@@ -1,15 +1,22 @@
+import sys
 import argparse
 import pandas as pd
+sys.path.append('../code')
 from utilities import dict_combiner
 from bump_inference_tests_ainner_ManyBumps import opt_wrapper
 from pdb import set_trace as bp
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--run_all", default=0, type=int)
+parser.add_argument("--id", default=0, type=int)
+FLAGS = parser.parse_args()
 
 mydict = {
     "target_lib_name": ["bumps_all"],
     "start": ["on", "off"],
     "m": [3, 5],
     "acc_opt": ["inner"],
-    "w_opt": ["inner", "outer""],
+    "w_opt": ["inner", "outer"],
     "single_beta": [1],
     "one_scale": [1],
     "plot_inner_opt": [0],  # [0, 1],
@@ -31,11 +38,11 @@ def run_main(sett):
     sett_K = {k.split('_')[0]: sett[k] for k in K_names}
 
     tune_names = ['maxiter_K', 'popsize_K', 'polish_K']
-    tune_dict = {k.split('_')[0]: sett[k] for k in tune_names}
+    sett['opt_settings_outer'] = {k.split('_')[0]: sett[k] for k in tune_names}
 
-    n_iters = sett_K['maxiter']*sett_K['popsize']*tune_dict['maxiter']*tune_dict['popsize']
+    n_iters = sett_K['maxiter']*sett_K['popsize']*sett['opt_settings_outer']['maxiter']*sett['opt_settings_outer']['popsize']
     if n_iters <= MAX_ITERS:
-        opt_wrapper(sett_K, tune_dict=tune_dict, **sett)
+        opt_wrapper(sett_K, tune_dict=sett)
 
 
 if __name__ == "__main__":
@@ -57,4 +64,4 @@ if __name__ == "__main__":
         bp()
     else:
         settings = EXPERIMENT_LIST[FLAGS.id]
-        run_main(**settings)
+        run_main(settings)
