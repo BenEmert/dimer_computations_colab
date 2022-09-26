e82cd630cf9a293ad3ae770d30a88f4617970124
mattlevine22 <mattlevine22@gmail.com>
2022-09-25 21:18:03 -0400

    upgrade hpc run to 3.5 using now-consistent MSE

--- run_scripts/inference_HPC/manybumps1switch_hpc.sh
+++ run_scripts/inference_HPC/manybumps1switch_hpc.sh
@@ -8,6 +8,6 @@
 #SBATCH --output=slurm/%x.%j.out
 #SBATCH --error=slurm/%x.%j.err
 
-base_dir="/groups/astuart/mlevine/dimer_computations_colab/results/ManyBumps1switch_3.4"
+base_dir="/groups/astuart/mlevine/dimer_computations_colab/results/ManyBumps1switch_3.5"
 
 srun python bump_inference_tests_ainner_ManyBumps_main.py --id $SLURM_ARRAY_TASK_ID --base_dir $base_dir
