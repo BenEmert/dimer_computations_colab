5168ce2c86ff40edaa46e0481cd5ac8ff72fc46e
mattlevine22 <mattlevine22@gmail.com>
2022-10-19 11:31:43 -0700

    add target summary extractor

---
+++ run_scripts/summarize_targets.py
@@ -0,0 +1,32 @@
+import os
+import pickle
+import numpy as np
+
+base = '/Users/matthewlevine/Downloads/hpc/dimers/BoundedBumps_1.0_m3/'
+dir = 'nswitches-2_nswitchlocs-5_a-inner_w-inner_singleBeta-1_scaleType-per-dimer_m-3_plotInner-0_targetID-{}/maxiterO-10_popsizeO-25_polishO-1_maxiterK-25_popsizeK-10_polishK-1_start-both/10182022230141_acc-inner_weights-inner/FinalK_{}/run0/model_info.pkl'
+
+D = 40 # number of input monomer concentrations
+N = 883 # number of targets
+S = 2 # number of optimizations per target
+
+targets = np.zeros((N,D))
+fits = np.zeros((N,D,S))
+
+for n in range(N):
+    for s in range(S):
+        fname = os.path.join(base, ndir.format(n, s))
+        try:
+            with open(fname, 'rb') as f:
+                x = pickle.load(f)
+
+            if n==0:
+                targets[n] = x['f_target']
+            fits[n,:,s] = x['f_fit']
+        except:
+            print('ID {}, round {} not available.'.format(n,s))
+            pass
+
+data = {'targets': targets, 'fits': fits}
+summary_fname = os.path.join(base, 'fit_summary.pkl')
+with open(summary_fname, 'a') as f:
+    pickle.dump(data, f)
