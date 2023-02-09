62651c69932f7a913dd9552d8994f4bbe9ec9710
mattlevine22 <mattlevine22@gmail.com>
2023-02-08 21:17:14 -0800

    adding simple code for studying linear combinations of dimers from large random networks

--- large_limit_code/run.sh
+++ large_limit_code/run.sh
@@ -3,14 +3,15 @@
 #Submit this script with: sbatch thefilename
 
 #SBATCH --time=1:00:00   # walltime
-#SBATCH --ntasks=5
+#SBATCH --n=5
 #SBATCH --output=slurm/%x.%j.out
 #SBATCH --error=slurm/%x.%j.err
 
 base_dir="/groups/astuart/mlevine/dimer_computations_colab/results/bigSims"
 
-srun python script.py --n_monomers 9 --base_dir $base_dir &
-srun python script.py --n_monomers 10 --base_dir $base_dir &
-srun python script.py --n_monomers 11 --base_dir $base_dir &
-srun python script.py --n_monomers 12 --base_dir $base_dir &
-srun python script.py --n_monomers 13 --base_dir $base_dir
+srun -n1 python script.py --n_monomers 9 --base_dir $base_dir &
+srun -n1 python script.py --n_monomers 10 --base_dir $base_dir &
+srun -n1 python script.py --n_monomers 11 --base_dir $base_dir &
+srun -n1 python script.py --n_monomers 12 --base_dir $base_dir &
+srun -n1 python script.py --n_monomers 13 --base_dir $base_dir
+wait
