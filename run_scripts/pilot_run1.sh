fcfa1e763b17e3cbebb69962ca0b457bf5908eb0
mattlevine22 <mattlevine22@gmail.com>
2023-04-01 12:09:42 -0700

    script for running first pilot

---
+++ run_scripts/pilot_run1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+conda activate dimer_opt2
+
+cd dimer_computations_colab/run_scripts
+
+dev_run=1
+n_random_Ks=3
+
+for m in 3 5 10;
+do
+  echo $m
+  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray.py --dev_run $dev_run --grid_dir badname --m $m --n_random_Ks $n_random_Ks > log_${m}.out 2> log_${m}.err
+done
