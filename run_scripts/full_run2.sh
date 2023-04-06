2dff6b0d00bb3075bc0503a3c0309f5b0e7f1156
mattlevine22 <mattlevine22@gmail.com>
2023-04-05 21:13:48 -0700

    set up big run with 50 Ks and 10% of targets used

---
+++ run_scripts/full_run2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# conda activate dimer_opt2
+
+dev_run=0
+n_random_Ks=50
+frac_targets=0.1
+base_dir='../results/fullrun2_0.1ofTargets'
+
+for m in 3 4 5 10 12;
+do
+  echo $m
+  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray.py --dev_run $dev_run --grid_dir badname --m $m --frac_targets $frac_targets --n_random_Ks $n_random_Ks --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
+done
