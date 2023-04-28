bff952569c1c04873aef177a49e707beedd75b85
mattlevine22 <mattlevine22@gmail.com>
2023-04-27 22:37:44 -0700

    oops fixing .py name

--- run_scripts/full_run2_2d.sh
+++ run_scripts/full_run2_2d.sh
@@ -9,5 +9,5 @@ base_dir='../results/fullrun2d'
 for m in 8;
 do
   echo $m
-  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray2d.py --dev_run $dev_run --grid_dir badname --m $m --n_random_Ks $n_random_Ks --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
+  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray_2D.py --dev_run $dev_run --grid_dir badname --m $m --n_random_Ks $n_random_Ks --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
 done
