c1f808fd3d3983b758076729183574a4c17c12cd
mattlevine22 <mattlevine22@gmail.com>
2023-06-01 19:12:21 -0700

    fix runner to set randomizeK=0

--- run_scripts/full_run2_specialKs_m8.sh
+++ run_scripts/full_run2_specialKs_m8.sh
@@ -10,5 +10,5 @@ base_dir='../results/specialK_m8'
 for m in 8;
 do
   echo $m
-  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray.py --dev_run $dev_run --grid_dir badname --m $m --frac_targets $frac_targets --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
+  nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray.py --randomizeK $randomizeK --dev_run $dev_run --grid_dir badname --m $m --frac_targets $frac_targets --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
 done
