a716c0e6fc1eedef14056cafe586d0d4e79cde67
mattlevine22 <mattlevine22@gmail.com>
2023-06-06 17:01:54 -0700

    adding full_run script that does m=2...13

--- run_scripts/full_run2.sh
+++ run_scripts/full_run2.sh
@@ -8,7 +8,7 @@ n_Ks=50
 frac_targets=0.1
 base_dir='../results/fullrun2_0.1ofTargets'
 
-for m in 3 4 5 10 12;
+for m in 2 3 4 5 6 7 8 9 10 11 12 13;
 do
   echo $m
   nohup python bump_inference_tests_ainner_BoundedBumpsSeparate_main_randomK_jacobTargets_perDimer_ray.py --dev_run $dev_run --grid_dir badname --m $m --frac_targets $frac_targets --n_Ks $n_Ks --randomizeK $randomizeK --base_dir $base_dir > log_${m}.out 2> log_${m}.err &
